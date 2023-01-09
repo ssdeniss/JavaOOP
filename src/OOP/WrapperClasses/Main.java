36347b270b2aa8688f2a97d38403719e8fb97e6d
Denis <denis.seremet@snt.md>
2023-01-09 12:22:05 +0200

    WrapperClasses

---
+++ src/OOP/WrapperClasses/Main.java
@@ -0,0 +1,12 @@
+package OOP.WrapperClasses;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 4;
+        Integer x2 = 123;
+        System.out.println(Integer.parseInt("432432"));
+
+        int x3 =  x2;
+        System.out.println(x3);
+    }
+}
