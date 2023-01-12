03503d01744465413a478d758985fbd2532aedaf
Denis <denis.seremet@snt.md>
2023-01-12 18:28:01 +0200

    Write to file

---
+++ src/OOP/WriteToFile/Main.java
@@ -0,0 +1,24 @@
+package OOP.WriteToFile;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class Main {
+    public static String pathFile(String path) {
+        String separator = File.separator;
+        return separator + "C:" + separator + "Users" + separator + "Admin" + separator + "Documents" + separator + "projects" +
+                separator + "JavaLessons" + separator + "JavaLessons" + separator + "src" + separator + "OOP" +
+                separator + "WriteToFile" + separator + path + ".txt";
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        PrintWriter writer = new PrintWriter(pathFile("testFile"));
+
+        writer.println("Write text 1 to file");
+        writer.println("Write text 2 to file");
+        writer.println("Write text 3 to file");
+        writer.close();
+    }
+}
